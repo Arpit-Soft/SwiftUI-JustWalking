68f5fb625d0dd9a8dc93269d87cfd4434c4c5a2b
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-29 22:07:57 +0530

    Initial Commit

---
+++ SwiftUI-JustWalking/SwiftUI_JustWalkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_JustWalkingApp.swift
+//  SwiftUI-JustWalking
+//
+//  Created by Arpit Dixit on 29/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_JustWalkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
