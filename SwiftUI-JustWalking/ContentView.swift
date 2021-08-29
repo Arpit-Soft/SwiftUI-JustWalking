ca9207fe063e1ae8b2ea71119f3b126f72a85717
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-29 23:04:51 +0530

    Added Total step count and distance travelled

--- SwiftUI-JustWalking/ContentView.swift
+++ SwiftUI-JustWalking/ContentView.swift
@@ -6,11 +6,46 @@
 //
 
 import SwiftUI
+import CoreMotion
 
 struct ContentView: View {
+    
+    @AppStorage("stepCount", store: UserDefaults(suiteName: "group.com.techzi.JustWalking"))
+    var stepCount = 0
+    
+    private let pedometer = CMPedometer()
+    @State private var steps: Int?
+    @State private var distance: Double?
+    private var isPedometerAvailable: Bool {
+        return CMPedometer.isPedometerEventTrackingAvailable() && CMPedometer.isDistanceAvailable() && CMPedometer.isStepCountingAvailable()
+    }
+    
+    private func intializePedometer() {
+        if isPedometerAvailable {
+            guard let startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date()) else {
+                return
+            }
+            pedometer.queryPedometerData(from: startDate, to: Date()) { (data, error) in
+                guard let data = data, error == nil else {return}
+                stepCount = data.numberOfSteps.intValue
+                steps = stepCount
+                distance = data.distance?.doubleValue
+                
+            }
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(steps ?? 0) steps")
+                .padding()
+            Text("\(String(format: "%.2f", distance ?? 0)) meters")
+                .padding()
+        }
+        
+        .onAppear(perform: {
+            intializePedometer()
+        })
     }
 }
 
